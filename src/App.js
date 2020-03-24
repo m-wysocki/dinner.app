0afacc5cfd5a05663e4e7bcec1dbec0069b8c932
Mateusz Wysocki <mateusz.wysocki53@gmail.com>
2020-03-24 20:40:08 +0100

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import MainTemplate from './templates/MainTemplate';
+import CategoriesView from './views/CategoriesView/CategoriesView';
 
 const App = () => (
-  <MainTemplate>
-    <h1>hello world</h1>
-  </MainTemplate>
+  <BrowserRouter>
+    <MainTemplate>
+      <h1>app</h1>
+      <ul>
+        <li>
+          <Link to="/">home</Link>
+        </li>
+        <li>
+          <Link to="/categories">categories</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/categories" component={() => <CategoriesView />} />
+      </Switch>
+    </MainTemplate>
+  </BrowserRouter>
 );
 
 export default App;
---
+++ src/views/CategoriesView/CategoriesView.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CategoriesView = () => {
+  return <div>test</div>;
+};
+
+export default CategoriesView;
