63dcd319055884946e7479db3e550dc2a7e97253
Mateusz Wysocki <mateusz.wysocki53@gmail.com>
2022-02-06 16:10:11 +0100

    v0.1 - add some style fixes

--- src/theme/GlobalStyle.js
+++ src/theme/GlobalStyle.js
@@ -9,6 +9,7 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
     font-family: 'Nunito', sans-serif;
   }
+
   html {
     font-size: 0.525vw;
     @media (max-width: 1499px) {
@@ -18,27 +19,33 @@ export const GlobalStyle = createGlobalStyle`
       font-size: 0.725vw;
     }
     @media ${theme.media.maxMobile} {
-        font-size: 10px;    
+      font-size: 10px;
     }
   }
+
   body {
-      font-size: 1.6rem;
-      margin: 0 0 10rem 0;
-      padding: 0;
-      color: #000000;
-      line-height: 1.5;
-      @media ${theme.media.maxMobile} {
-        font-size: 1.2rem 
-      }
+    font-size: 1.6rem;
+    margin: 0 0 10rem 0;
+    padding: 0;
+    color: #000000;
+    line-height: 1.5;
+    @media ${theme.media.maxMobile} {
+      font-size: 1.2rem
+    }
   }
-  
-  a{
+
+  a {
     text-decoration: none;
   }
-  
-  img{
+
+  img {
     max-width: 100%;
   }
+  
+  .isModalOpen {
+    overflow: hidden;
+  }
+
   .isModalOpen::after {
     content: '';
     background-color: rgba(0, 0, 0, 0.7);
@@ -48,16 +55,25 @@ export const GlobalStyle = createGlobalStyle`
     bottom: 0;
     left: 0;
     right: 0;
+    z-index: 9;
   }
+
   .Toastify__toast-container {
     width: 500px;
     max-width: 95vw;
     text-align: center;
+
+    @media ${theme.media.maxMobile} {
+      max-width: unset;
+      width: 100%;
+    }
   }
+
   .Toastify__toast--success {
     background: ${theme.colors.green};
     color: ${theme.colors.text};
   }
+
   .Toastify__close-button {
     color: ${theme.colors.text};
   }
